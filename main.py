6caac858f2288243a5a13c4aa577aa95ec79a199
jpcsantana <joaop401@outlook.com>
2025-10-14 19:01:57 -0300

    feat: adicionando main.py com instruções para realização do projeto

---
+++ main.py
@@ -0,0 +1,20 @@
+# Linha 1: Título e equipe
+# Linha 2: Introdução ao projeto (objetivo, escopo)
+# Comentários abaixo serão removidos posteriormente
+
+# ORGANIZAÇÃO: Cada membro pode criar uma parte do CRUD, tendo um membro com pelo menos dois tipos de ação. 
+# Os commits serão feitos em branches específicas para a funcionalidade a ser criada.
+# Os membros que ficaram com apenas um tipo de ação podem escolher também entre ajudar na especificação dos menus do sistema
+# e verificar se todos os requisitos pedidos foram atingidos.
+
+# DOCUMENTAÇÃO: A documentação pode ser feita em conjunto e com IA
+# Podemos incluir o passo a passo para execução do sistema, além da explicação dos componentes
+
+# ESTRUTURA: O sistema deve ser modular, separando bem cada etapa em funções
+# É interessante que tudo no código seja tipado, para fins de legibilidade e organização
+# Se for necessário, podemos criar arquivos separados para dividir os métodos utilizados
+# Esse arquivo será o responsável por definir o fluxo do usuário, é importante que ele seja o mais enxuto possível
+
+# JOÃO: Read e Delete
+# GABRIEL:
+# MURILO: 
