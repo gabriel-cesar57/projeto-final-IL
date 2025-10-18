409606676ce1463d9267c68ebdf40d31fa905e3c
jpcsantana <joaop401@outlook.com>
2025-10-18 18:04:29 -0300

    corrigindo organizacao do projeto e adicionando metodo para buscar via id

--- operacoes/leitura_tarefas.py
+++ operacoes/leitura_tarefas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from auxiliares import auxiliares
 
 # Visualizar todas as tarefas;
 # Visualizar tarefa baseado em id;
@@ -6,33 +7,36 @@ from datetime import datetime
 # Visualizar tarefas vencidas;
 # Visualizar tarefas de um único tipo de prioridade.
 
-def visualizar_todas_tarefas(lista_tarefas: list) -> None:
-    for tarefa in lista_tarefas:
-        print(f'{'='*5} TAREFA {tarefa["id"]} {'='*5}\n')
-        print(f'Descrição: {tarefa['descricao']}')
-        print(f'Data de entrega: {tarefa['data_entrega']}')
-        print(f'Prioridade da tarefa: {tarefa['prioridade']}')
-        print(f'Foi concluída?: {'Sim' if tarefa['concluida'] else 'Não'}')
-        print(f'{'='*15}')
-
 def visualizar_tarefa_por_id(lista_tarefas: list, id: int) -> None:
-    print()
+    tarefa = next((t for t in lista_tarefas if t['id'] == id), None)
+
+    if tarefa:
+        auxiliares.visualizar_unica_tarefa(tarefa)
+    else:
+        print(f'Nenhuma tarefa com o ID {id} foi encontrada.')
 
 def visualizar_tarefas_vencidas(lista_tarefas: list) -> None:
-    tarefas_vencidas = [t for t in lista_tarefas if t['concluida'] == False]
+    hoje = datetime.now()
+    tarefas_vencidas = []
+
+    for tarefa in lista_tarefas:
+        data_entrega = auxiliares.formatar_data_datetime(tarefa['data_entrega'])
+
+        if(not tarefa['concluida'] and data_entrega < hoje):
+            tarefas_vencidas.append(tarefa)
 
     if(len(tarefas_vencidas) == 0):
         print('Nenhuma tarefa vencida.')
     else:
-        visualizar_todas_tarefas(tarefas_vencidas)
+        auxiliares.visualizar_lista_tarefas(tarefas_vencidas)
 
 def visualizar_tarefas_prioridade(lista_tarefas: list, prioridade: str) -> None:
     tarefas_prioridade = [t for t in lista_tarefas if t["prioridade"].lower() == prioridade.lower()]
 
     if(len(tarefas_prioridade) == 0):
-        print('Nenhuma tarefa encontrada com essa prioridade.')
+        print('Nenhuma tarefa encontrada com a prioridade indicada.')
     else:
-        visualizar_todas_tarefas(tarefas_prioridade)
+        auxiliares.visualizar_lista_tarefas(tarefas_prioridade)
 
 def visualizar_tarefas_proximas(lista_tarefas: list) -> None:
     print()
