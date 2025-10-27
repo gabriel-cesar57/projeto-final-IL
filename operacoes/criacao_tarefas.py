02f65500996fcb2f9387c5911c0c858ff1fe7af8
gabriel-cesar57 <gabrielcesargomes04@gmail.com>
2025-10-27 19:41:01 -0300

    fix: ajustando o codigo do create

--- operacoes/criacao_tarefas.py
+++ operacoes/criacao_tarefas.py
@@ -1,41 +1,40 @@
 import datetime
+from auxiliares import auxiliares
 
+# ENUM PRIORIDADE: 
+# 1 - Baixa
+# 2 - Média
+# 3 - Alta
 
-
+#lista temporaria. Só para teste.
 lista_tarefas = [
     {
         "id": 1,
         "descricao": "Estudar Python",
         "data_entrega": "2024-06-30",
-        "prioridade": "alta",
+        "prioridade": 3,
         "concluida": False
     }
 ]
 
 # função para criar uma nova tarefa
 
-def criar_tarefa(titulo: str, data_entrega: datetime.date | str) -> dict:
+def criar_tarefa(titulo: str, data_entrega: datetime) -> None:
     """Cria uma nova tarefa com título e data de entrega."""
-    if isinstance(data_entrega, datetime.date):
-        data_entrega_str = data_entrega.isoformat()
-        """ Converte datetime.date para string no formato ISO (YYYY-MM-DD) """
-    else:
-        data_entrega_str = data_entrega
-        """ Assume que já é uma string no formato correto """
+    # verificar se os parametros != nulo ou vazio
+    # verificar se data > hoje
+    # prioridade das tarefas com numeros
+    
     nova_tarefa = {
         "id": len(lista_tarefas) + 1,
         "descricao": titulo,
-        "data_entrega": data_entrega_str,
+        "data_entrega": data_entrega,
         "prioridade": "média",
         "concluida": False
     }
     lista_tarefas.append(nova_tarefa)
-    return nova_tarefa
+    auxiliares.visualizar_unica_tarefa(nova_tarefa)
 
 # Exemplo de uso
 nova = criar_tarefa("Fazer exercícios", datetime.date(2024, 7, 5))
 print(nova)
-
-
-
-
