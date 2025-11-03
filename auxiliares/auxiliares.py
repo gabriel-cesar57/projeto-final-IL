9ff70c99ccdb541d0f992484f762be3cd549dc8b
Gabriel Cesar <gabrielcesargomes04@gmail.com>
2025-11-03 20:24:10 -0300

    update: Correção de erros

--- auxiliares/auxiliares.py
+++ auxiliares/auxiliares.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 def formatar_data_datetime(data_nao_formatada: str)  -> datetime:
     return datetime.strptime(data_nao_formatada, '%d/%m/%Y')
--- operacoes/criacao_tarefas.py
+++ operacoes/criacao_tarefas.py
@@ -1,5 +1,5 @@
 import datetime
-from auxiliares import auxiliares
+from auxiliares.auxiliares import validar_parametros, visualizar_unica_tarefa
 
 # ENUM PRIORIDADE: 
 # 1 - Baixa
@@ -8,33 +8,33 @@ from auxiliares import auxiliares
 
 #lista temporaria. Só para teste.
 lista_tarefas = [
-    {
-        "id": 1,
-        "descricao": "Estudar Python",
-        "data_entrega": "2024-06-30",
-        "prioridade": 3,
-        "concluida": False
-    }
-]
+     {
+         "id": 1,
+         "descricao": "Estudar Python",
+         "data_entrega": "2024-06-30",
+         "prioridade": 3,
+         "concluida": False
+     }
+    ]
 
 # função para criar uma nova tarefa
 def criar_tarefa(titulo: str, data_entrega: datetime, prioridade: int) -> None:
     #Cria uma nova tarefa com título e data de entrega, e prioridade opcional (padrão 2 - Média).
     if prioridade is None:
         prioridade = 2  # Prioridade padrão é Média
-    auxiliares.validar_parametros(titulo, data_entrega, prioridade, None)
+    validar_parametros(titulo, data_entrega, prioridade, None)
     
     nova_tarefa = {
-        "id": len(lista_tarefas) + 1,
+        "id": len(lista_tarefas) + 1, # percorre a lista_tarefas e puxa o ID mais recente, depois, soma +1 para a nova tarefa
         "descricao": titulo,
         "data_entrega": data_entrega,
         "prioridade": prioridade,
         "concluida": False
     }
     lista_tarefas.append(nova_tarefa)
-    auxiliares.visualizar_unica_tarefa(nova_tarefa)
+    visualizar_unica_tarefa(nova_tarefa)
     return nova_tarefa
 
 # Exemplo de uso
-nova = criar_tarefa("Fazer exercícios", datetime.date(2025, 12, 5), 2)
-print(nova)
+nova = criar_tarefa("Fazer atividades", datetime.date(2025, 12, 5), 2)
+# para rodar o código, execute com o comando phyton - m operacoes.criacao_tarefas
